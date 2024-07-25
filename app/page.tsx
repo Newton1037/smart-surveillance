297ebcf15b0ea7f9a6eb49b5e458041b331b4226
Newton1037 <nitinkumar1037nk@gmail.com>
2024-07-26 01:23:02 +0530

    today

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 import { ModeToggle } from '@/components/theme-toggle'
 import { Button } from '@/components/ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { Separator } from '@/components/ui/separator'
-import { FlipHorizontal } from 'lucide-react'
+import { Slider } from '@/components/ui/slider'
+import { Camera, FlipHorizontal, PersonStanding, Video, Volume2 } from 'lucide-react'
 import React, { useRef, useState } from 'react'
+import { Rings } from 'react-loader-spinner'
 import Webcam from 'react-webcam'
+import { toast } from 'sonner'
 
 type Props = {}
 
@@ -13,6 +17,28 @@ const page = (props: Props) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   const [mirrored , setMirrored] = useState<boolean>(false)
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
+  const [volume, setVolume] = useState(0.8)
+
+  function promptScreenshot(){
+
+  }
+
+  function recordVideo(){
+
+  }
+
+  function toggleAutoRecord(){
+    if(autoRecordEnabled){
+      setAutoRecordEnabled(false)
+      toast("Auto record disabled")
+    }else{
+      setAutoRecordEnabled(true)
+      toast("Auto record enabled")
+    }
+  }
+
   return (
     <div className="flex h-screen">
       
@@ -27,6 +53,8 @@ const page = (props: Props) => {
       {/* {Right section with all buttons} */}
       <div className="flex flex-row flex-1">
         <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
+
+           {/* {upper section} */}
            <div className="flex flex-col gap-2">
              <ModeToggle />
              <Button variant="outline" size="icon">
@@ -37,12 +65,57 @@ const page = (props: Props) => {
              <Separator className="my-2" />
            </div>
 
+           {/* {middle section} */}
            <div className="flex flex-col gap-2">
-             <Separator />
+             <Separator className="my-2" />
+
+             <Button 
+                variant={"outline"} 
+                size={"icon"} 
+                onClick={promptScreenshot}
+              >
+                <Camera />
+              </Button>
+             <Button 
+                variant={isRecording ? "destructive" : "outline"} 
+                size={"icon"} 
+                onClick={recordVideo}
+             >
+              <Video />
+             </Button>
+             <Button 
+                variant={autoRecordEnabled ? "destructive" : "outline"} 
+                size={"icon"} 
+                onClick={toggleAutoRecord}
+             >
+              {autoRecordEnabled ? <Rings color="white" height="45" /> : <PersonStanding />}
+             </Button>
+             
+             <Separator className="my-2" />
            </div>
 
+           {/* {lower section} */}
            <div className="flex flex-col gap-2">
              <Separator />
+
+             <Popover>
+              <PopoverTrigger>
+                <Button variant="outline" size="icon">
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider 
+                  min={0} 
+                  max={1} 
+                  step={0.2}
+                  defaultValue={[volume]}
+                  onValueCommit={(val) => {
+                    setVolume(val[0])
+                  }} 
+                />
+              </PopoverContent>
+             </Popover>
            </div>
         </div>
       </div>
